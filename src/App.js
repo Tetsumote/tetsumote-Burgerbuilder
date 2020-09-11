b1a2a34ccffa60734269b1bdc3aa15ca72689a2f
Paweł Grajewski <unionproject88@gmail.com>
2020-09-11 23:11:56 +0200

    End of Project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./styles.css";
 
-import Layout from "./components/Layout/Layout";
+import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder/>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
